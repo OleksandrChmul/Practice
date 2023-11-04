558aa0fcdca903f412742520b73a52947af1fa31
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-11-04 14:50:48 +0200

    1

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-// /**
-//  *? Використовуючи функцію if...else,
-//  *? напишіть код, який запитуватиме:
-//  *? "Яка офіційна назва JavaScript?"
-//  *? Якщо користувач вводить "ECMAScript",
-//  *? то показати через alert: "Вірно!"
-//  *? інакше відобразити:"Не знаєте? ECMAScript!"
-//  */
+/* Напишіть функцію, яка приймає об'єкт і рахує загальну суму зарплат працівників. */
 
-// const answer = prompt("Яка офіційна назва JavaScript?").toLowerCase().trim();
-// if (answer === "ecmascript") {
-//     alert("Вірно!");
-// }
-// else {
-//     alert("Не знаєте? ECMAScript!");
-//     }
-// console.log(answer);
-/**
- *
- *
- *? Напишіть цикл, який виводить у консоль
- *? всі парні числа  від max до min  за спаданням
- */
-const max = 50;
-const min = 23;
-for (let i = max; i >= min; i -= 1) {
-  if (!(i % 2)) console.log(i);
+const someObj = {
+  worker1: 360,
+  worker2: 750,
+  worker3: 240,
+};
+function sumTotalSelery(obj) {
+  const arrayValues = Object.values(obj);
+  let total = 0;
+  for (let i of arrayValues) {
+    total += i;
+  }
+  return total;
 }
+console.log(sumTotalSelery(someObj));
